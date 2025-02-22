1aaa36cb05a59d3a39b545b662a8fd923de5f698
Ryan Ronish <roni0210@bears.unco.edu>
2025-02-21 20:52:06 -0700

    header and footer year additions to copyright

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -3,9 +3,8 @@ class Footer {
     render() {
       return `
         <footer class="footer">
-          <p>&copy; ${new Date().getFullYear()} stockThots &copy; A Ryan Ronish Production</p>
+          <p>&copy; ${new Date().getFullYear()} stockThots &copy; ${new Date().getFullYear()} A Ryan Ronish Production</p>
         </footer>
       `;
     }
   }
-  
